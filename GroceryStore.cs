a1889ea29dca5ff53fda87c963760fefb82975f6
CodyD <codyd536@gmail.com>
2019-06-04 16:29:03 -0400

    Add files via upload

---
+++ GroceryStore.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace joke {
+    class Program {
+        static void Main(string[] args) {
+            WriteJokeLine("A programmer talks to his wife..");
+            TalkToWife();
+        }
+
+
+        public static void TalkToWife() {
+            WriteJokeLine("His wife says, go to the grocery store");
+            GoToGroceryStore();
+        }
+
+        public static void GoToGroceryStore() {
+            bool isAtGroceryStore = true;
+            WriteJokeLine("and pick up some milk!");
+            PickUpMilk();
+            WriteJokeLine("While you're there, pick up some eggs");
+            while (isAtGroceryStore) {
+                PickUpEggs();
+            }
+
+            WriteJokeLine("He never returned...");
+            return;
+        }
+
+        public static void PickUpMilk() {
+            Console.WriteLine("I got the milk!");
+        }
+
+        public static void PickUpEggs() {
+            Console.WriteLine("I got the eggs!");
+        }
+
+        public static void WriteJokeLine(string line) {
+            Console.WriteLine(line);
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
